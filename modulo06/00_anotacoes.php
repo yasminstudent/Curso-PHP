46285b8f5ee1cfebc45b93f7cf0e0e66357a020d
yasmin <yasminprog02@gmail.com>
2021-11-25 20:14:53 -0300

    propriedades tipadas, metodo construtor e encapsulamento

--- modulo06/00_anotacoes.php
+++ modulo06/00_anotacoes.php
@@ -46,5 +46,68 @@
 
   $post1 = new Post()
   $post1->aumentaLike(); //likes do post1 passa a valer 1
+
+
+  ----------------------- Typed Properties (php 7.4) -----------------------
+  https://alunos.b7web.com.br/curso/php/typed-properties-74
+
+  Definir o tipo da variável p/ aceitar somente valores desse tipo
+  Ex:
+    public int $likes = 0;
+    public array $comments = [];
+    public string $author;
+
+
+  ----------------------- Método Construtor -----------------------
+  https://alunos.b7web.com.br/curso/php/metodo-construtor
+
+  Esse método é executado sempre que um objeto é criado
+  Sua sintaxe:
+  public function __construct(){
+    //código
+  }
+
+  Ele pode receber parâmetros
+  Ex:
+  public function __construct($qtLikes = 0){
+    $this->likes = $qtLikes
+  }
+
+  $post1 = new Post(25)
+
+  ----------------------- Entendendo Encapsulamento -----------------------
+  https://alunos.b7web.com.br/curso/php/entendendo-encapsulamento
+
+  Métodos getters e setters
+
+  Sintaxe/Exemplo:
+  public function setAuthor($author){
+    $this->author = $author;
+  }
+
+  public function getAuthor(){
+    return $this->author
+  }
+
+  $post1 = new Post()
+  $post1->setAuthor('Yasmin');
+  echo $post1->getAuthor();
+
+  Usa-se esses métodos p/ fazer tratamentos com o parâmetro recebido, se necessário
+  $post1->author = 'Yasmin'; //dessa forma não seria possível realizar o tratamento
+
+  E então com o uso desses métodos é possível/viável deixar o atributo privado:
+  private string $author;
+
+  Exemplos de tratamentos:
+  public function setAuthor($author){
+    if(strlen($author) >= 3){
+      $this->author = $author;
+    }
+  }
+
+  public function getAuthor(){
+    return $this->author ?? ''; //tratamento se não tiver nenhum autor
+  }
 */
 ?>
