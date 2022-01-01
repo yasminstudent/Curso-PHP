861a4897540f80ddcc87ba80906033a1f05920ad
yasminstudent <yasminprog02@gmail.com>
2022-01-01 12:45:45 -0300

    comecando conceito solid

---
+++ modulo07/04_conceito_solid/teoria.php
@@ -0,0 +1,10 @@
+<?php
+/*
+    SOLID (p/ evitar complicações ao fazer manutenções)
+    S -> Single Responsibility Principle (Princípio da responsabilidade única)
+    O -> Open-Closed Principle (Princípio Aberto-Fechado)
+    L -> Liskov Substitution Principle (Princípio de Substituição de Liskov)
+    I -> Interface Segregation Principle (Princípio da segregação da Interface)
+    D -> Dependency Inversion Principle (Princípio da inversão da dependência)
+*/
+?>
